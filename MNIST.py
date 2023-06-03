b415639e7b2bc5d6388ac0b9e4f0ed23efe2f287
DESKTOP-C5UVM8B\coffee <tkdqja15@gmail.com>
2023-06-03 20:44:56 +0900

    MNIST모델 생성 및 앞으로의 계획

--- MNIST.py
+++ MNIST.py
@@ -1,29 +1,32 @@
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.optimizers import Adam
 import matplotlib.pyplot as plt
-# MNIST 데이터셋 로드
+
+mnist = tf.keras.datasets.mnist
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# 데이터 전처리
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32') / 255.0
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32') / 255.0
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
-
-# 모델 구성
 model = Sequential()
-model.add(Flatten(input_shape=(28, 28, 1)))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.2))
+model.add(Conv2D(input_shape=(28, 28, 1), kernel_size=3, filters = 64))
+model.add(MaxPooling2D(pool_size=(2,2),strides=(1,1)))
+model.add(Conv2D(kernel_size=3, filters = 32))
+model.add(MaxPooling2D(pool_size =(2,2), strides=(1,1)))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0,2))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0,2))
 model.add(Dense(10, activation='softmax'))
 
-# 모델 컴파일
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# 모델 훈련
-history = model.fit(x_train, y_train, batch_size=128, epochs=30, validation_data=(x_test, y_test))
+model.summary()
+
+history = model.fit(x_train, y_train, epochs=5, validation_split=0.1)
 
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
@@ -40,5 +43,6 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend(['Train', 'Validation'], loc='upper right')
 plt.show()
-# 모델 저장
+
+model.evaluate(x_test, y_test)
 model.save('mnist_model.h5')
